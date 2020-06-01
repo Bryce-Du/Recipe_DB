aac59225d9cb985443ef1f80173bfb740ffe74d6
Bryce-Du <63598874+Bryce-Du@users.noreply.github.com>
2020-06-01 10:53:18 -0400

    add create action logic

--- app/controllers/ingredient_controller.rb
+++ app/controllers/ingredient_controller.rb
@@ -7,9 +7,21 @@ class IngredientController < ApplicationController
         erb :"/ingredients/new"
     end
     post "/ingredients/new" do
-        ingredient = Ingredient.find_or_create_by_name(params["ingredient"]["name"])
-        ingredient.calories ||= params["ingredient"]["calories"] # if ingredient already exists, don't update calories, should be changed in edit instead.
-        ingredient.save
-        
+        /# technically, if the user already has an item in their pantry, they should just be updating the quantity with edit
+        but they shouldn't be expected to know what they already have. #/
+        ingredient = Ingredient.find_by(name: params["ingredient"]["name"])
+        if !!ingredient
+            users_ingredient = UsersIngredient.find_by(ingredient_id: ingredient.id)
+            if !!users_ingredient
+                users_ingredient.quantity += params[users_ingredient][quantity]
+                users_ingredient.save
+            else
+                UsersIngredient.create(user_id: current_user.id, ingredient_id: ingredient.id, quantity: params[users_ingredient][quantity])
+            end
+        else 
+            ingredient = Ingredient.create(params["ingredient"])
+            UsersIngredient.create(user_id: current_user.id, ingredient_id: ingredient.id, quantity: params[users_ingredient][quantity])
+        end
+        redirect to "/ingredients/list"
     end
 end
