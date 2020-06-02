d8174cf3b3d542e6df533b0cc42b510832abf1b4
Bryce-Du <63598874+Bryce-Du@users.noreply.github.com>
2020-06-02 11:10:31 -0400

    finish create action

--- app/controllers/recipe_controller.rb
+++ app/controllers/recipe_controller.rb
@@ -7,6 +7,15 @@ class RecipeController < ApplicationController
         erb :"/recipes/new"
     end
     post "/recipes/new" do
-        binding.pry
+        # unlike ingredients where an item of the same name should have the same info, 
+        # two different recipes might exist that are called the same thing, but prepared in a unique way
+        recipe = Recipe.create(params["recipe"])
+        # for each ingredient listed, find_or_create the ingredient, and then create the join model
+        params["recipes_ingredient"].each do |recipe_ingredient|
+            RecipesIngredient.create(ingredient_id: Ingredient.find_or_create_by(name: recipe_ingredient["ingredient"]["name"]).id, quantity: recipe_ingredient["quantity"], recipe_id: recipe.id)
+        end
+        # assign the recipe to the user
+        UsersRecipe.create(recipe_id: recipe.id, user_id: current_user.id)
+        redirect to "/recipes"
     end
 end
