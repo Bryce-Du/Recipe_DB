2b02c0736fbb369c9ba350faccd85a17a8228a56
Bryce-Du <63598874+Bryce-Du@users.noreply.github.com>
2020-06-05 09:05:39 -0400

    make batch add ingredient, improve ui

--- app/controllers/ingredient_controller.rb
+++ app/controllers/ingredient_controller.rb
@@ -23,18 +23,22 @@ class IngredientController < ApplicationController
     post "/ingredients/new" do
         /# technically, if the user already has an item in their pantry, they should just be updating the quantity with edit
         but they shouldn't be expected to know what they already have. #/
-        ingredient = Ingredient.find_by(name: params["ingredient"]["name"])
-        if !!ingredient
-            users_ingredient = UsersIngredient.find_by(ingredient_id: ingredient.id)
-            if !!users_ingredient
-                users_ingredient.quantity += params["users_ingredient"]["quantity"].to_i
-                users_ingredient.save
-            else
-                UsersIngredient.create(user_id: current_user.id, ingredient_id: ingredient.id, quantity: params["users_ingredient"]["quantity"])
+        ingredients = params["ingredient"]
+        ingredients.each_with_index do |ingredient_hash, index|
+            quantity = params["users_ingredient"][index]["quantity"].to_i
+            ingredient = Ingredient.find_by(name: ingredient_hash["name"])
+            if !!ingredient
+                users_ingredient = UsersIngredient.find_by(ingredient_id: ingredient.id)
+                if !!users_ingredient
+                    users_ingredient.quantity += quantity
+                    users_ingredient.save
+                else
+                    UsersIngredient.create(user_id: current_user.id, ingredient_id: ingredient.id, quantity: quantity)
+                end
+            else 
+                ingredient = Ingredient.create(ingredient_hash)
+                UsersIngredient.create(user_id: current_user.id, ingredient_id: ingredient.id, quantity: quantity)
             end
-        else 
-            ingredient = Ingredient.create(params["ingredient"])
-            UsersIngredient.create(user_id: current_user.id, ingredient_id: ingredient.id, quantity: params["users_ingredient"]["quantity"])
         end
         redirect to "/ingredients"
     end
@@ -45,7 +49,11 @@ class IngredientController < ApplicationController
     patch "/ingredients/:id/edit" do
         @ingredient = Ingredient.find(params[:id])
         @users_ingredient = UsersIngredient.where(:user_id=>current_user.id).find_by(:ingredient_id=>params[:id])
-        @users_ingredient.update(params["users_ingredient"])
+        if params["users_ingredient"]["quantity"].to_i == 0 #if quantity reduced to zero, remove from pantry
+            UsersIngredient.delete(UsersIngredient.where(:user_id=>current_user.id).find_by(:ingredient_id=>params[:id]).id)
+        else
+            @users_ingredient.update(params["users_ingredient"])
+        end
         @ingredient.update(params["ingredient"])
         redirect to "/ingredients"
     end
