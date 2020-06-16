153132432e431a128fb66447deed4f34f04d0b8a
Bryce-Du <63598874+Bryce-Du@users.noreply.github.com>
2020-06-16 14:14:03 -0400

    Update recipe.rb

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -8,12 +8,10 @@ class Recipe < ActiveRecord::Base
         self.ingredients.map{|i| i.calories}.sum
     end
     def can_be_made_by?(user)
-        recipes_ingredients = RecipesIngredient.where(recipe_id: self.id)
-        users_ingredients = UsersIngredient.where(user_id: user.id)
         can_make = true
-        recipes_ingredients.each do |recipe_ingredient|
+        self.recipes_ingredients.each do |recipe_ingredient|
             # first find if the user has any of that item
-            users_ingredient = users_ingredients.detect{|i| i.ingredient_id == recipe_ingredient.ingredient_id}
+            users_ingredient = user.users_ingredients.detect{|i| i.ingredient_id == recipe_ingredient.ingredient_id}
             if !users_ingredient || users_ingredient.quantity < recipe_ingredient.quantity # if the user doesn't have it or doesn't have enough of it
                 can_make = false
             end
@@ -21,10 +19,8 @@ class Recipe < ActiveRecord::Base
         can_make
     end
     def make_recipe(user)
-        recipes_ingredients = RecipesIngredient.where(recipe_id: self.id)
-        users_ingredients = UsersIngredient.where(user_id: user.id)
-        recipes_ingredients.each do |recipe_ingredient|
-            users_ingredient = users_ingredients.detect{|i| i.ingredient_id == recipe_ingredient.ingredient_id}
+        self.recipes_ingredients.each do |recipe_ingredient|
+            users_ingredient = user.users_ingredients.detect{|i| i.ingredient_id == recipe_ingredient.ingredient_id}
             if users_ingredient.quantity > recipe_ingredient.quantity # if the user has more than enough, remove that amount
                 users_ingredient.quantity -= recipe_ingredient.quantity
                 users_ingredient.save
